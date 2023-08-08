3879b16449869534e5043548f8aa8153ed6b4ee5
Ngare <ngaremaina4@gmail.com>
2023-08-08 14:12:43 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ function App() {
         <Route path="/upd-prop" element = {<UpdateProperty/>}/>
         </Routes> 
     </BrowserRouter> 
-    <Footer/> 
+    {/* <Footer/>  */}
     </div>
   );
 }
