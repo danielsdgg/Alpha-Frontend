4730e404d38ff6cfa29e20a748fe91ae0486972b
mohamed <mohamed.yusuf@student.moringaschool.com>
2023-08-08 11:50:47 -0400

    solving problem

--- src/App.js
+++ src/App.js
@@ -1,113 +1,60 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Front from "./components/Front";
 import About from "./components/About";
-import PropertyList from "./components/PropertyList"
+import PropertyList from "./components/PropertyList";
 import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./components/Profile";
 import Details from "./components/Details";
-import  Login  from "./components/Login";
-import  Register  from "./components/Register";
-import Booking from "./components/Booking";
-import UpdateProperty from "./components/UpdateProperty";
 import LoginForm from './LoginForm';
+
 function App() {
+  const handleLoginSuccess = (responseData) => {
+    // Handle login success, e.g., set user data, show dashboard, etc.
+    console.log('Login successful:', responseData);
+  };
 
-  const App = () => {
-    const handleLoginSuccess = (responseData) => {
-         console.log('Login successful!', responseData);
-};
+  // Define property and image states
+  const [property, setProperty] = useState([]);
+  const [image, setImages] = useState([]);
 
-
-return (
-  <div>
-    {/* Other components */}
-    <LoginForm onLoginSuccess={handleLoginSuccess} />
-  </div>
-);
-};
-  // ddei3mzex
-
-  const uploadProfile = (file) => {
-    const data = new FormData()
-    data.append('cloudname','ddei3mzex')
-    data.append('upload_preset','profile_upload')
-    data.append('file',file.file)
-
-    fetch(`https://api.cloudinary.com/v1_1/demo/image/upload`,{method:"POST",
-    body:data
-  })
-  .then((r) => r.json())
-  .then((data) => {console.log(data)})
-  }
-
-  // curl https://api.cloudinary.com/v1_1/demo/image/upload -X POST --data 'file=sample.jpg&timestamp=173719931&api_key=436464676&signature=a781d61f86a6f818af'
-
-
-  const [property, setProperty] = useState([])
-  const [image, setImages] = useState([])
-
-
+  // Fetch data for property and image states
   useEffect(() => {
     fetch('http://127.0.0.1:5000/properties')
-    .then((r) => r.json())
-    .then((data) => setProperty(data))
-  },[])
+      .then((r) => r.json())
+      .then((data) => setProperty(data))
+  }, []);
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/images')
-    .then((r) => r.json())
-    .then((data) => setImages(data))
-  },[])
-
-  console.log(image)
-
-  function loginUser(email,pass){
-    fetch('http://127.0.0.1:5000/login',{
-        method: "POST",
-        headers: {
-            'Accept':'application/json',
-            'Context-Type':'application/json',
-        },
-        body: JSON.stringify({ email,pass}),
-    })
-    .then((r) => {
-        if (r.ok){
-            alert("logged in Successfully")
-            return r.json()
-        }
-    })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.error('Error:',error);
-        console.log('Response:',error.response);
-    });
-  }
-
-
+      .then((r) => r.json())
+      .then((data) => setImages(data))
+  }, []);
 
   return (
-    <div className="App">       
-    <BrowserRouter>  
-    <NavBar/> 
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
         <Routes>
-        <Route exact path="/" element= {<Login/>}/>
-        <Route exact path="/front" element= {<Front/>}/>
-        <Route exact path="/register" element= {<Register/>}/>
-        <Route path="/about" element = {<About/>}/>
-        <Route path="/contacts" element = {<Contacts/>}/>
-        <Route path="/properties" element = {<PropertyList property= {property} image = {image}/>} />
-        <Route path="/profile" element = {<Profile/>}/>
-        <Route path="/details/:id" element = {<Details/>}/>
-        <Route path="/booking" element = {<Booking/>}/>
-        <Route path="/upd-prop" element = {<UpdateProperty/>}/>
-        </Routes> 
-    </BrowserRouter> 
-    <Footer/> 
+          <Route path="/" element={<Front />} />
+          <Route path="/front" element={<Front />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          {/* Pass property and image as props to PropertyList */}
+          <Route path="/properties" element={<PropertyList property={property} image={image} />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/details/:id" element={<Details />} />
+
+          {/* Routes for Login and Register */}
+          <Route path="/login" element={<LoginForm onLoginSuccess={handleLoginSuccess} />} />
+          {/* You can add a similar route for Register component here */}
+
+          {/* Other components or elements */}
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
