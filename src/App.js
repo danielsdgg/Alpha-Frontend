5e53069f6d1d8c5ef3c834c1803bfba1f0dbac06
arnold <musaliokello@gmail.com>
2023-08-08 11:16:52 +0300

    Search

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import  Login  from "./components/Login";
 import  Register  from "./components/Register";
 import Booking from "./components/Booking";
 import UpdateProperty from "./components/UpdateProperty";
+import SearchProperty from "./components/SearchProperty";
 
 function App() {
   // ddei3mzex
@@ -41,6 +42,26 @@ function App() {
     .then((r) => r.json())
     .then((data) => setProperty(data))
   },[])
+
+  const handleSearch = (filteredProperties) => {
+    // Extract the location, property type, and maximum price from the first property in the filteredProperties array
+    const location = filteredProperties[0].location;
+    const propertyType = filteredProperties[0].property_type;
+    const Price = parseFloat(filteredProperties[0].price); 
+  
+    // Filter properties based on the same location, property type, and  price
+    const sameLocationTypeAndPriceProperties = property.filter(
+      propertyItem => (
+        propertyItem.location.toLowerCase() === location.toLowerCase() &&
+        propertyItem.property_type === propertyType &&
+        parseFloat(propertyItem.price) <= Price
+      )
+    );
+  
+    // Set the filtered properties in the state
+    filteredProperties(sameLocationTypeAndPriceProperties);
+  };
+  
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/images')
@@ -91,6 +112,7 @@ function App() {
         <Route path="/details/:id" element = {<Details/>}/>
         <Route path="/booking" element = {<Booking/>}/>
         <Route path="/upd-prop" element = {<UpdateProperty/>}/>
+        <Route path="/search" element={<SearchProperty properties={property} onSearch={handleSearch} />} />
         </Routes> 
     </BrowserRouter> 
     <Footer/> 
