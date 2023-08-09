c8c056eeaeb37a9b0011c1cdde535858c7a99d7f
arnold <musaliokello@gmail.com>
2023-08-09 10:32:20 +0300

    merge

--- src/App.js
+++ src/App.js
@@ -37,8 +37,7 @@ function App() {
   // curl https://api.cloudinary.com/v1_1/demo/image/upload -X POST --data 'file=sample.jpg&timestamp=173719931&api_key=436464676&signature=a781d61f86a6f818af'
 
 
-  const [property, setProperty] = useState([])
-
+ 
   useEffect(() => {
     fetch('https://final-projects-huib.onrender.com/properties')
     .then((r) => r.json())
